ccbbe69e527f76970cf7a87a4361b6745cb22d9b
Rutvik72 <shethrutvik@gmail.com>
2024-08-09 17:43:38 -0400

    removed cohere for GenAi and added caption generation

--- builder.py
+++ builder.py
@@ -1,26 +1,43 @@
 import random
 import os
 import requests
-import cohere
+import google.generativeai as genai
+# import cohere
 
-quote = None
+import json
+
+# Constants
+API_KEY = "AIzaSyDv0bV2dWMdtEgJOxcCfiWr0lLHlb3QU2U"
 
 def getQuote():
     response = requests.get('https://zenquotes.io/api/random')
     quote = response.json()[0]
+    # print(quote)
     return quote
 
-def getCaption():
-    inputMessage = "Caption for the following quote, first caption then hashtags: " + quote['q'] + " - " + quote['a']
-    co = cohere.Client("2haSGJan1MkH76qxSSVGsqQ3uPc1cIsD5hcIT5vi")
-    response = co.chat(
-        message=inputMessage
-    )
+def getCaption(quote):
+    inputMessage = quote['q'] + " - " + quote['a']
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel('gemini-1.5-flash', generation_config={"response_mime_type": "application/json"})
+
+    prompt = quote['q'] + " - " + quote['a'] + """
+        Create a caption for the followiong quote:
+        
+        Using this JSON scheme:
+            Caption Text = {"caption_text", str},
+            Hashtags = {"hashtags": str}
+
+        1. caption should be only text without hashtag, 
+        2. hashtag should only have hashtags
+    """
+
+    response = model.generate_content(prompt).text
+    caption = parseMessageText(response)
     return response
 
-def parseMessageText():
-    # caption = messageText + hashtag
-    return
+def parseMessageText(response):
+    response_json = json.loads(response)
+    return response_json['caption_text'] + " " + response_json['hashtags']
 
 def getRandomVideo():
     return
@@ -30,6 +47,7 @@ def getRandomMusic():
 
 def build():
     # get all the elements
+    print(getCaption(getQuote()))
 
     # build video from the elements
     return
